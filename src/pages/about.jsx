42b4bbc9bffa333173ff6765d9cebaa89eb3173d
hellonewworldjs2023 <hello.new.worldjs.2023@gmail.com>
2023-02-15 14:28:54 +0900

    refactor:使用されていない変数の削除

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import { Main } from 'src/components/Main/Main'
 import { Header } from 'src/components/Header/Header';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
