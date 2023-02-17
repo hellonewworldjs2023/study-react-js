6bedafb4b7774bac36d27733262ef15ffd485e99
hellonewworldjs2023 <hello.new.worldjs.2023@gmail.com>
2023-02-17 11:27:32 +0900

    new features:カウンターを設置して、useStateでカウントアップする。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,31 +1,26 @@
 import Head from 'next/head'
 import { Main } from 'src/components/Main/Main'
 import { Header } from 'src/components/Header/Header'
-import { useCallback  } from 'react';
-import { useEffect } from 'react';
-import Link from 'next/link';
-
-
+import { useEffect, useState } from 'react';
 
 
 export default function Home() {
-  const foo = 1;
+  const [count,setCount] = useState(1);
+  // let foo = 1;
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  },[]);
+  const handleClick = (e) => {
+    // foo = foo + 1;
+    setCount((count) => count + 1);
+  };
   
   useEffect(()=>{
-    console.log("マウント時")
     document.body.style.backgroundColor = "lightblue";
 
     return() =>{
-      console.log("アンマウント時")
       document.body.style.backgroundColor = ""; 
     }
-  },[])
+  },[]);
+  console.log(count);
 
 
   return (
@@ -36,12 +31,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Header />
-      <Link
-        href='/about' 
-        onClick={handleClick}
-      >
-        ボタン
-      </Link>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main path="index" 
       />
     </>
